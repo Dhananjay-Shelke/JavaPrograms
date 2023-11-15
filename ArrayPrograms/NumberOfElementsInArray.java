c7f0efb32f3931a0a653006eb380cf46ebcbd553
Dhananjay Shelke <147627140+Dhananjay-Shelke@users.noreply.github.com>
2023-11-15 23:17:27 +0530

    push

---
+++ ArrayPrograms/NumberOfElementsInArray.java
@@ -0,0 +1,9 @@
+package core;
+//print the number of elements present in an array
+public class NumberOfElementsInArray {
+	public static void main(String[] args) {
+		int arr[] = {10,20,15,440,25,49,60,78,1200};
+		System.out.println("Length of Array : "+arr.length);
+	}
+
+}
